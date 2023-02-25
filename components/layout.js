18645910ab4a2351cf1e9d34b7365df5b5afd335
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-02-25 19:04:58 +0000

    deployment still not working

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link';
-import bavProfilePic from '../public/images/bavProfilePic.jpg'
+import bavProfilePic from './bavProfilePic.jpg'
 
 const name = 'Bav Nagarajah';
 export const siteTitle = 'Next.js Blog';
