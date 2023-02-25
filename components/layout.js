5581c7c329ebf2f4313cd32917ac4817aa9226fe
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-02-25 18:56:46 +0000

    for production build

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link';
-import bavPic from '../public/images/bavProfilePic.JPG'
+import bavPic from '../public/images/bavProfilePic.jpg'
 
 const name = 'Bav Nagarajah';
 export const siteTitle = 'Next.js Blog';
